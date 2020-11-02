830932b1dbba4ec57312f5f02a19e09bee2d2b50
Andrea Burattin <andrea.burattin@gmail.com>
2020-11-02 11:29:43 +0100

    Reshaping of the menu

--- src/components/ProcessesList.vue
+++ src/components/ProcessesList.vue
@@ -1,14 +1,14 @@
 <template>
-    <ul class="nav flex-column mb-2">
-        <li v-bind:key="proc.id" v-for="proc in processes" class="nav-item">
+    <div class="list-group list-group-flush">
             <router-link
-                class="nav-link select-process"
+                v-bind:key="proc.id"
+                v-for="proc in processes"
+                class="list-group-item list-group-item-action bg-light border-0"
                 :to="{ name: 'ProcessView', params: { id: proc.id }}">
                 <font-awesome-icon icon="file" />
                 Process {{ proc.name }}
             </router-link>
-        </li>
-    </ul>
+    </div>
 </template>
 
 <script>
--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,4 @@
-module.exports = { };
+module.exports = {
+  publicPath: '/plg/'
+};
   
